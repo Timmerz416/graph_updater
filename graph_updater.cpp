8207b91cc8b73f8091193224642f66af40078344
Tim Lampman <timothy.lampman@gmail.com>
2015-02-16 09:28:22 -0500

    Reduced the number of graphs updated consistent with the website

--- graph_updater.cpp
+++ graph_updater.cpp
@@ -8,15 +8,11 @@ using namespace std;
 
 int main(void) {
     // Create the list of urls to call
-    int n = 7;
+    int n = 3;
     vector<string> url_list(n);
     url_list[0] = "indoor_temperature_graph.php";
     url_list[1] = "outdoor_temperature_graph.php";
-    url_list[2] = "luminosity_graph.php";
-    url_list[3] = "pressure_graph.php";
-	url_list[4] = "lux_graph.php";
-	url_list[5] = "heating_graph.php";
-	url_list[6] = "humidity_graph.php";
+    url_list[3] = "lux_graph.php";
 
     // Set the clock initialization
     timespec interval;
